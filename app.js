a4f614ed7a157181454cc68fd769332ca9d988d2
Mark Nair <marknair@me.com>
2021-11-15 14:39:09 -0600

    Form redirect

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req)
+    const url = req.url;
+    res.setHeader("Content-Type", "text-html")
+
+        if (url === '/') {
+            res.write('<html>');
+            res.write('<head><title>Enter a message</title></head>');
+            res.write('<body><form action = "/message" method = "post"><input type = "text"><button type="submit">Send</button></form></body>');
+            res.write('</html>');
+            return res.end();
+        }
+
+
+        res.write('<html>');
+        res.write('<head><title>My Awesome Page</title></head>');
+        res.write('<body><h1>Hello from Node Server</h1></body>');
+        res.write('</html>');
+        res.end()
 });
 
 server.listen(3000)
